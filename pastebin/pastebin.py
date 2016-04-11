8f83b86c406bf792989c763f69cb07129cb1b95d
Mariusz Kozakowski <11mariom+wordpress@gmail.com>
2016-04-11 20:04:41 +0200

    update due to changes in Paste class

--- pastebin/pastebin.py
+++ pastebin/pastebin.py
@@ -1,12 +1,36 @@
 # -*- encoding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+from database import db, init_db
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+#db = SQLAlchemy(app)
+
+from paste import Paste
 
 @app.route("/")
 def hello_world():
     return "hello world"
 
-#if __name__ == "__main__":
+@app.route("/paste/<id>")
+def get_paste(id):
+    p = Paste()
+    return p.get(id).get_data()
+
+@app.route("/paste/", methods = ["POST"])
+def put_paste():
+    data = request.get_data()
+    print(data)
+    p = Paste()
+    return "pasted to id: %s" % (p.put(data))
+
 def main():
     app.run()
+
+if __name__ == "__main__":
+    app.debug = True
+    init_db()
+    main()
